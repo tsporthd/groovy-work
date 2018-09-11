69bbf4d2e11a817589b4fe3f66ae33fca5d8c69d
Larry L Presswood <larry.presswood@citizensbank.com>
2018-09-11 17:40:15 -0400

    Add new script to show unsealing and access of secrets

--- src/main/groovy/VaultInitDriver.groovy
+++ src/main/groovy/VaultInitDriver.groovy
@@ -1,37 +1,41 @@
 def outputDir = "/home/olbdev/vault/vaultData"
 def rootUrl = "http://localhost:8200"
+def devRole = 'dev-role'
+def policyFile = '/home/olbdev/projects/groovy-work/src/main/resources/devPolicy.json'
+def vaultPolicy = 'dig-dev'
+
 
 Vault vault = new Vault(outputDir: outputDir, baseUrl: rootUrl)
 
 def keys = vault.initVault()
-println(keys)
+println("KEYS-> $keys")
 
 
 def unseal = vault.unseal(keys[Vault.ROOT_TOKEN], keys[Vault.UNSEAL_KEYS], 3)
 println("Vault Seal Status $unseal")
 
 
-def policyFile = '/home/olbdev/projects/groovy-work/src/main/resources/devPolicy.json'
-def vaultPolicy = 'dig-dev'
+
 def policy = vault.setPolicy(keys[Vault.ROOT_TOKEN], vaultPolicy, policyFile)
-println(policy)
+println("POLICY RETURN: $policy")
 
 def appRole = vault.setAppRole(keys[Vault.ROOT_TOKEN])
-println(appRole)
+println("APP ROLE RETURN: $appRole")
 
-def devRole = 'dev-role'
+
 def roleAssign = vault.setDevRole(keys[Vault.ROOT_TOKEN], devRole, vaultPolicy)
-println(roleAssign)
+println("ROLE ASSIGN RETURN: $roleAssign")
+
 
 def roleId = vault.getRoleId(keys[Vault.ROOT_TOKEN], devRole)
-println(roleId)
+println("ROLEID: $roleId")
 
 def secret = vault.getSecret(keys[Vault.ROOT_TOKEN], devRole)
-println(secret)
+println("SECRET: $secret")
 
 
 def clientSecret = vault.appRoleLogin(roleId, secret)
-println(clientSecret) // used as token for access to secrets
+println("CLIENT SECRET: clientSecret") // used as token for access to secrets
 
 def secretsFile = '/home/olbdev/projects/groovy-work/src/main/resources/devSecrets.json'
 vault.writeSecrets(clientSecret, secretsFile)
@@ -39,3 +43,12 @@ vault.writeSecrets(clientSecret, secretsFile)
 
 println(vault.readSecret(clientSecret,"mw-x2-dev"))
 println(vault.readSecret(clientSecret,"mw-m1-dev"))
+
+
+def sealResult = vault.sealVault(keys[Vault.ROOT_TOKEN])
+println("Vault seal return $sealResult")
+
+
+
+
+
---
+++ src/main/groovy/VaultUnsealDriver.groovy
@@ -0,0 +1,24 @@
+def outputDir = "/home/olbdev/vault/vaultData"
+def rootUrl = "http://localhost:8200"
+def devRole = 'dev-role'
+
+
+// This would be done prior to service launch to ensure vault is unsealed
+Vault vault = new Vault(outputDir: outputDir, baseUrl: rootUrl)
+def keys = vault.initVault()
+println("KEYS-> $keys")
+def unseal = vault.unseal(keys[Vault.ROOT_TOKEN], keys[Vault.UNSEAL_KEYS], 3)
+println("Vault Seal Status $unseal")
+def roleId = vault.getRoleId(keys[Vault.ROOT_TOKEN], devRole)
+println("ROLEID: $roleId")
+def secret = vault.getSecret(keys[Vault.ROOT_TOKEN], devRole)
+println("SECRET: $secret")
+
+
+
+// Normal access to secrets assuming vault is unsealed
+def clientSecret = vault.appRoleLogin(roleId, secret)
+println("CLIENT SECRET: clientSecret") // used as token for access to secrets
+
+println(vault.readSecret(clientSecret, "mw-x2-dev"))
+println(vault.readSecret(clientSecret, "mw-m1-dev"))
